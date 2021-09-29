64eadd060ce5e92007c75e9a8c61362d36d1b7c1
shogo <st6502st@gmail.com>
2021-09-29 17:55:58 +0900

    test

--- service_worker.js
+++ service_worker.js
@@ -41,3 +41,35 @@ function fetchData() {
     };
   };
 }
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      // 重要：リクエストを clone する。リクエストは Stream なので
+      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+      // 必要なので、リクエストは clone しないといけない
+      let fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // 重要：レスポンスを clone する。レスポンスは Stream で
+        // ブラウザ用とキャッシュ用の2回必要。なので clone して
+        // 2つの Stream があるようにする
+        let responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
